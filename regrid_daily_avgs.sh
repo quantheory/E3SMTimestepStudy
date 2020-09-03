58e697bb487c8dc4dc39e46cdcde89433060a4f2
Sean Patrick Santos <SeanPatrickSantos@gmail.com>
2020-09-03 12:41:39 -0700

    Regrid script mild improvements

--- regrid_daily_avgs.sh
+++ regrid_daily_avgs.sh
@@ -3,10 +3,13 @@
 REMAP_FILE=/usr/gdata/climdat/maps/map_ne30np4_to_fv128x256_aave.20160301.nc
 IN_DIR=/p/lscratchh/santos36/timestep_daily_avgs
 OUT_DIR=/p/lscratchh/santos36/timestep_daily_avgs_lat_lon
-CASE_NAMES="timestep_presaer_ZM_10s timestep_MG2_10s timestep_CLUBB_10s timestep_dyn_10s"
+CASE_NAMES="timestep_all_rad_10s"
+DAYS="20 21 22 23 24 25 26 27 28 29 30"
 
 for case_name in $CASE_NAMES; do
     date
     echo "On case $case_name"
-    ncremap -m $REMAP_FILE -O $OUT_DIR $IN_DIR/$case_name.0001-01-*.nc
+    for day in $DAYS; do
+        ncremap -m $REMAP_FILE -O $OUT_DIR $IN_DIR/$case_name.0001-01-$day.nc
+    done
 done
--- regrid_monthly_avgs.sh
+++ regrid_monthly_avgs.sh
@@ -4,10 +4,14 @@ REMAP_FILE=/usr/gdata/climdat/maps/map_ne30np4_to_fv128x256_aave.20160301.nc
 IN_DIR=/p/lscratchh/santos36/timestep_monthly_avgs
 OUT_DIR=/p/lscratchh/santos36/timestep_monthly_avgs_lat_lon
 CASE_NAMES="timestep_ctrl timestep_all_10s"
+MONTHS="01 02"
 
 for case_name in $CASE_NAMES; do
     date
     echo "On case $case_name"
-    ncremap -m $REMAP_FILE -O $OUT_DIR $IN_DIR/$case_name.0001-*.nc
-    ncremap -m $REMAP_FILE -O $OUT_DIR $IN_DIR/$case_name.0002-*.nc
+    for month in $MONTHS; do
+        date
+        echo "On month $month"
+        ncremap -m $REMAP_FILE -O $OUT_DIR $IN_DIR/$case_name.0004-$month.nc
+    done
 done
